9e1b418dd6943b60802077a3d0e6a0a5525a754e
DevMeister007 <paakwesidebrah@gmail.com>
2022-09-08 19:56:14 -0700

    added advanced tasks for project 0x00 C

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - prints to standard err
+ * Return: 1 if success, failure if otherwise
+ */
+int main(void)
+{
+ 	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
