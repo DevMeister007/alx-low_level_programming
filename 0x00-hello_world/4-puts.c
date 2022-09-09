b1b80e61c61ab1dd715ab646adb3b44623b73814
DevMeister007 <paakwesidebrah@gmail.com>
2022-09-08 17:18:52 -0700

    added all tasks for project 0x00 C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#!/bin/bash
-/* including libraries*/
 #include <stdio.h>
-#include <string.h>
-/** Main will print string fed to puts()
-  * and will return null character if fail
+
+/**
+ * Main will print string fed to puts()
+ * and will return null character if fail
  */
-int main(){
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
