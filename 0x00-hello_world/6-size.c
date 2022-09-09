bd79c642bc5e9f93f873a696c17419badedfde48
DevMeister007 <paakwesidebrah@gmail.com>
2022-09-08 19:04:13 -0700

    added all tasks for project 0x00 C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - point of entry and will print
+ * respective sizes of specified data types
+ * return 0 if success otherwise failed
+ */
+ int main (void)
+ {
+ 	printf("size of a char: %d byte(s)\n", sizeof(char));
+	printf("size of an int: %d byte(s)\n", sizeof(int));
+	printf("size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
+}
