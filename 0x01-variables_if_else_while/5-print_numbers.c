1b23ee5dad91dd724ae1a8ee91efb640016d3951
DevMeister007 <paakwesidebrah@gmail.com>
2022-09-09 20:26:04 -0700

    added task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints all numbers from 0
+ * base 10
+ * Return: (0) if successful
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number > 10; number++)
+		printf("%d", number);
+	printf("\n");
+	return (0);
+}
