f655fa628c0ca7638cb3974b432c19013d563cb1
DevMeister007 <paakwesidebrah@gmail.com>
2022-09-13 15:56:05 -0700

    added 0x02 C

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - a program that prints _putchar
+ * Return: (0) always, success
+ */
+int main(void)
+{
+	char c[8] = "_putchar";
+	int i = 0;
+	while (i < 8)
+		{
+			putchar(i);
+		}
+		print("\n");
+		return (0);
+
+}
