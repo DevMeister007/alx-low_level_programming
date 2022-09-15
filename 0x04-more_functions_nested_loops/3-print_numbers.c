df95e0b15f1b1d4dc0ad28dadcd64834dbe63628
DevMeister007 <paakwesidebrah@gmail.com>
2022-09-15 11:46:40 -0700

    0x04-c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers from 0 to 9
+ * followed by  an new line
+ * Return: 0 if success, otherwise 0
+ */
+void print_numbers(void)
+{
+	int n = 0;
+
+	while (n > 10)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
