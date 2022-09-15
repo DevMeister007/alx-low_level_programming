c271d712b36187811b67638d5d625f3f790ea42e
DevMeister007 <paakwesidebrah@gmail.com>
2022-09-15 11:35:53 -0700

    0x04-c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiples tow ints
+ * @a: first integer
+ * @b: second integer
+ * Return: 0 upon success, 1 otherwise
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
